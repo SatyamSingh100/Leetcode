70beef349480d752fdf0d48eb74775c04e1e868a
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-02-18 19:03:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.59%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int minimumPushes(string a) {
-        int n = a.size();
-        // unordered_map<char,int> m;
-        // for(int i=0; i<n; i++){
-        //     if(m.find(a[i]) == m.end()) m[a[i]] =1;
-        //     else m[a[i]]++;
-        // }
-        if(n<9) return n;
-        else if(n>=9 && n<=16) return 8 + (n-8)*2;
-        else if(n>16 && n<=24) return 24 + (n-16)*3;
-        else return 48 + (n-24)*4;
-        return -1;
+        int n = a.size(),ans=0;
+        for(int i=0; i<n; i++){
+            ans += (i/8) +1;
+        }
+        return ans;
     }
 };
