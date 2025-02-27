dbdcd809242596a6d06a32bf72960eaa0aaeab0c
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-02-27 20:38:52 +0530

    Time: 63 ms (35.60%) | Memory: 27.1 MB (60.48%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+
+// This problem is exactly similar to LIS(longest inc subsequence), only thing ie changing here is that we can choose pair in any order. So, first sort the vector; & then simply run the LIS Loop;
+// LIS ----> For Bottom-Up we dont copy the recursion code here but what we do is:   For every 'i' we take 'j' from 0-(i-1) and get the larget possible subsequence for every 'i' and then we store it in a "ans" variable...
+
+
     int findLongestChain(vector<vector<int>>& a) {
         int n= a.size(),ans=1;
         sort(begin(a), end(a));
