ca26f10a5b11c19bd9cf3e0626ee9ffa041a7531
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-03-01 14:13:22 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (62.06%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& a) {
+        int n= a.size(),cnt=0;
+        vector<int> v,p(n);
+        for(int i=0; i<n-1; i++){
+            if(a[i] == a[i+1]){
+                a[i] = a[i] *2;
+                a[i+1] = 0;
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(a[i] == 0) cnt++;
+            else v.push_back(a[i]);
+        }
+        for(int i=0; i<cnt; i++) v.push_back(0);
+        return v;
+    }
+};
