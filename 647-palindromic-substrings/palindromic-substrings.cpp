90fb705606415626a8cafb75026331898bbf8056
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-03-04 14:13:35 +0530

    Time: 1727 ms (5.11%) | Memory: 487.7 MB (5.21%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -12,7 +12,6 @@ public:
     } 
     int countSubstrings(string s) {
         int n = s.size(),cnt=0;
-        int i = 0,j=n-1;
         for(int i=0; i<n;i++){
             for(int j=i; j<n; j++){
                 int len = (j-i+1);
