02af1801b0f0953929e31cc2db0fdf3d1add117a
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-03-04 14:05:08 +0530

    Time: 8 ms (46.07%) | Memory: 13.1 MB (60.07%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -14,18 +14,16 @@ public:
         // int ans = f(s,t,0,0);
         // return ans;
         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        
         for(int i=0; i<=n; i++){
-            dp[i][0] = i;
-        }
-        for(int i=0; i<=m; i++){
-            dp[0][i] = i;
-        }
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
+            for(int j=0; j<=m; j++){
+                if(i == 0 || j == 0) dp[i][j] = i+j;
+                else{
                 int pick = 1e9;
                 if(s[i-1] == t[j-1]) pick = dp[i-1][j-1];
                 int npick = min((1 + dp[i-1][j-1]),min(1 + dp[i-1][j], 1 + dp[i][j-1]));
                 dp[i][j] = min(pick,npick);
+                }
             }
         }
         return dp[n][m];
