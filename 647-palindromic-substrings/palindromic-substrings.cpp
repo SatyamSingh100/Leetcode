4fc2fd593c7354c940a80097f20870f5d5c463a7
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-03-23 14:03:22 +0530

    Time: 1771 ms (5.00%) | Memory: 487.7 MB (5.13%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     bool check(string s){
-        int i = 0, j = s.size()-1;
+        int n= s.size();
+        if(n==1) return true;
+        int i=0,j=n-1;
         while(i<j){
             if(s[i] != s[j]) return false;
-            else{
-                i++; j--;
-            }
+            i++; j--;
         }
         return true;
-    } 
+    }
     int countSubstrings(string s) {
-        int n = s.size(),cnt=0;
-        for(int i=0; i<n;i++){
+        int n= s.size(),cnt=0;
+        for(int i=0; i<n; i++){
             for(int j=i; j<n; j++){
-                int len = (j-i+1);
-                string st = s.substr(i,len);
-                if(check(st) == true) cnt++; 
+                string str = s.substr(i,j-i+1);
+                if(check(str) == true) cnt++;
             }
         }
         return cnt;
