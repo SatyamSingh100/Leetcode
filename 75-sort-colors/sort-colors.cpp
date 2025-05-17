e539fc1dbafdc9af7af7378b70f8b98010ccbd6b
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-05-17 08:53:55 +0530

    Time: 7 ms (0.92%) | Memory: 11.4 MB (97.40%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        // int l=0,m=0,h=nums.size()-1;
+        // while(m<=h){
+        //     if(nums[m] == 0){
+        //         swap(nums[l],nums[m]);
+        //         l++,m++;
+        //     }
+        //     else if(nums[m] == 1){
+        //         m++;
+        //     }
+        //     else if(nums[m] == 2){
+        //         swap(nums[m],nums[h]);
+        //         h--;
+        //     }
+        // }
+        // for(auto it:nums){
+        //     cout<<it<<" ";
+        // }
+        
+
+        //..Bubble Sort
+        int n = nums.size();
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(nums[j] > nums[j+1]) swap(nums[j],nums[j+1]);
+            }
+        }
+        for(auto it:nums) cout<<it<<" ";
+    }
+};
