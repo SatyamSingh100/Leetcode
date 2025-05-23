2a9b8a9625f922eb8ae083610f5c2bf82f4a8174
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-05-23 17:38:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.25%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        //Dutch National Flag Algo.. (DNF Algo)
-        int n = nums.size();
+    void sortColors(vector<int>& a) {
+        int n = a.size();
         int l=0,m=0,h=n-1;
         while(m<=h){
-            if(nums[m] == 0) swap(nums[m],nums[l]), l++, m++;
-            else if(nums[m] == 1) m++;
-            else swap(nums[m],nums[h]), h--;
+            if(a[m] == 0){
+                swap(a[m],a[l]);   l++; m++;
+            }
+            else if(a[m] == 1) m++;
+            else if(a[m] == 2){
+                swap(a[m],a[h]);    h--;
+            }
         }
-        for(auto it:nums) cout<<it<<" ";
-
-
-        //..Bubble Sort
-        // int n = nums.size();
-        // for(int i=0; i<n-1; i++){
-        //     for(int j=0; j<n-i-1; j++){
-        //         if(nums[j] > nums[j+1]) swap(nums[j],nums[j+1]);
-        //     }
-        // }
-        // for(auto it:nums) cout<<it<<" ";
     }
 };
