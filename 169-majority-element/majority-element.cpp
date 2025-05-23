7b6c6444934c35cc33e7b1ccecb978e23a38d2b2
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-05-23 17:55:14 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (58.54%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int majorityElement(vector<int>& a) {
+        //Moore's Voting Algorithn..
         int n = a.size();
-        unordered_map<int,int> m;
-        for(int i=0; i<n; i++){
-            m[a[i]]++;
+
+        //Assume the first element as Majority Element and make its cnt as 1
+        int me = a[0],cnt=1;
+
+        //Run a loop till n-1
+        for(int i=1; i<n; i++){
+
+            //If the current element is the same as Maj. Elem, then simply inc its cnt by 1
+            if(a[i] == me) cnt++;
+            //Else dec by 1;
+            else{
+                cnt--;
+            }
+            //And if the cnt becomes 0,then change ur majority elem to a[i] and make its cnt as 1
+            if(cnt == 0){
+                me = a[i]; cnt = 1;
+            }
         }
-        for(auto it: m){
-            int num1 = it.first,num2 = it.second;
-            if(num2> n/2) return num1;
-        }
-        return -1;
+        return me;
+
     }
 };
