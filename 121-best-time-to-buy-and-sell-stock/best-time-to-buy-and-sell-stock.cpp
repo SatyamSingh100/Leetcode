3848d060d5369c4932263902b3e1a3272886e114
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-05-24 18:19:46 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (26.51%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int maxProfit(vector<int>& a) {
-        int n= a.size(),sum = 0;
-        if(n == 1) return 0;
-        int i=0,j=1;
-        while(j<n && (j-i)>=1){
-            sum = max(sum,a[j]-a[i]);
-            if(a[j] - a[i] < 0){
-                i = j;
-            }
-            j++;
+        // int n= a.size(),sum = 0;
+        // if(n == 1) return 0;
+        // int i=0,j=1;
+        // while(j<n && (j-i)>=1){
+        //     sum = max(sum,a[j]-a[i]);
+        //     if(a[j] - a[i] < 0){
+        //         i = j;
+        //     }
+        //     j++;
+        // }
+        // return sum;
+        int mini = 1e9,maxPro=0,n=a.size();
+        for(int i=0; i<n; i++){
+            mini = min(mini,a[i]);                            //This will get our minm till a[i]
+            maxPro = max(maxPro,a[i]-mini);          //and this will generate the maxm ie a[i] - mini
         }
-        return sum;
-        
-        
+        return maxPro;
     }
 };
