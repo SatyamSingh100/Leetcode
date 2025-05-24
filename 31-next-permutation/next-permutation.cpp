866310cea4f34a43d6e317b83ffbef074985e97c
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-05-24 18:40:09 +0530

    Time: 15 ms (1.60%) | Memory: 15.6 MB (67.59%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& a) {
-        int n = a.size();
-        int piv = -1;
-        for(int i= n-2; i>=0; i--){
-            if(a[i] < a[i+1]){
-                piv = i; break;
-            }
-        }
-        if(piv == -1) {
-            reverse(begin(a),end(a));
-            return;
-        }
-
-        for(int i= n-1; i>piv; i--){
-            if(a[i] > a[piv]){
-                swap(a[i],a[piv]);
+        int pivot=-1;
+        int n= a.size();
+        for(int i=n-2; i>=0; i--){
+            if(a[i]<a[i+1]){
+                pivot =i;
                 break;
             }
         }
-        sort(a.begin() + piv+1, a.end());
+        if(pivot==-1){
+            reverse(a.begin(),a.end());
+            return;
+        }
+        for(int i=n-1; i>pivot; i--){
+            if(a[i]>a[pivot]){
+                swap(a[i],a[pivot]);
+                break;
+            }
+        }
+        int i=pivot+1,j=n-1;
+        while(i<j){
+            swap(a[i],a[j]);
+            i++,j--;
+        }
+        for(auto it:a){
+            cout<<it<<" ";
+        }
     }
 };
