27b093296961dde46d2d54c0ca9c86c04155937b
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-05-27 18:06:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (37.53%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        bool sign = true;
+        if(dividend == divisor) return 1;
+        long ans =0;
+        if(dividend > 0 && divisor<0) sign = false;
+        if(dividend < 0 && divisor > 0) sign  = false;
+
+        long n = labs(dividend);
+        long d = labs(divisor);
+        while(n>=d){
+            int cnt =0;
+            while(n >= (d << (cnt+1))){
+                cnt++;
+            }
+            ans += 1 << cnt;
+            n = n - (d<<cnt);
+        }
+
+        if(ans == 1<<31 && sign == true) return INT_MAX;
+        if(ans == 1<<31 && sign == false) return INT_MIN;
+        return sign? ans : (-ans);
+    }
+};
