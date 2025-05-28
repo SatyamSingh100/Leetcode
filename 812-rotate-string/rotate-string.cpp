1e0e35bc0401cb5a2523b54ee914f9ea3931a76e
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-05-28 17:24:07 +0530

    Time: 3 ms (6.36%) | Memory: 8.5 MB (21.31%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -2,13 +2,20 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         int n = s.size();
-        for(int i=1; i<=n; i++){
-            string temp = "",curr = s;
-            for(int j=0; j<i; j++) temp += curr[j];
-            for(int j=0; j<(n-i); j++) curr[j] = curr[j+i];
-            int idx=0;
-            for(int j = n-i; j<n; j++) curr[j] = temp[idx++];
-            if(curr == goal) return true;
+        // for(int i=1; i<=n; i++){
+        //     string temp = "",curr = s;
+        //     for(int j=0; j<i; j++) temp += curr[j];
+        //     for(int j=0; j<(n-i); j++) curr[j] = curr[j+i];
+        //     int idx=0;
+        //     for(int j = n-i; j<n; j++) curr[j] = temp[idx++];
+        //     if(curr == goal) return true;
+        // }
+        // return false;
+
+         s = s+s;
+        for(int i=0; i<=n; i++){
+            string g = s.substr(i,n);
+            if(g == goal) return true;
         }
         return false;
     }
