49a25bf1d4ab4f556609305927f2fadcadf19811
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-05-29 23:51:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (28.22%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -25,11 +25,11 @@ public:
             dp[0][j] = 1;
         }
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(a[i][j] == 1) dp[i][j] = 0;
-            }
-        }
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(a[i][j] == 1) dp[i][j] = 0;
+        //     }
+        // }
 
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
