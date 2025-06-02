a77bd225710c444cb23ea47c19c506b7b5cbf9d4
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-06-02 22:12:17 +0530

    Time: 151 ms (28.60%) | Memory: 17.5 MB (47.78%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int sum=0;
-        int n=nums.size();
-        for(int i=0; i<n; i++) sum+=nums[i];
-        if(sum%2!=0) return false;
+    bool canPartition(vector<int>& a) {
+        int n = a.size(),sum=0,k=0;
+        for(int i=0; i<n; i++) sum += a[i];
+        if(sum & 1) return false;
         else{
-            int tar = sum/2;
-            vector<vector<bool>> dp(n,vector<bool>(tar+1,0));
-            for(int i=0; i<n; i++) dp[i][0] =1;
-            if(nums[0] <=tar) dp[0][nums[0]] =1;
-            for(int i=1;i<n; i++){
-                for(int j=1; j<=tar; j++){
-                    bool npick= dp[i-1][j];
-                    bool pick =0;
-                    if(j>=nums[i]) pick = dp[i-1][j-nums[i]];
-                    dp[i][j] = npick | pick;
-                }
+            k = sum/2;
+        vector<vector<bool>> dp(n,vector<bool>(k+1,0));
+        for(int i=0; i<n; i++) dp[i][0] = true;
+        if(k >= a[0]) dp[0][a[0]] = true;
+        for(int i=1; i<n; i++){
+            for(int j=1; j<=k; j++){
+                bool ntake = dp[i-1][j];
+                bool take = false;
+                if(j >= a[i]) take = dp[i-1][j-a[i]];
+
+                dp[i][j] = ntake | take;
             }
-            return dp[n-1][tar];
         }
+        return dp[n-1][k];
+        }
+        return false;
     }
 };
