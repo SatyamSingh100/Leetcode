540b66230d2e261492e73d79fea1f4e17fb51441
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-06-15 23:44:37 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (27.58%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,25 +11,39 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        vector<int> v;
-        if(head == NULL || head->next == NULL) return head; 
-        ListNode* temp = head;
-        while(temp != NULL && temp->next != NULL){
-            v.push_back(temp->val);
-            temp = temp->next->next;
+        // vector<int> v;
+        // if(head == NULL || head->next == NULL) return head; 
+        // ListNode* temp = head;
+        // while(temp != NULL && temp->next != NULL){
+        //     v.push_back(temp->val);
+        //     temp = temp->next->next;
+        // }
+        // if(temp != NULL) v.push_back(temp->val);
+        // temp = head->next;
+        // while(temp != NULL && temp->next != NULL){
+        //     v.push_back(temp->val);
+        //     temp = temp->next->next;
+        // }
+        // if(temp != NULL) v.push_back(temp->val);
+        // temp = head; int idx =0;
+        // while(temp != NULL){
+        //     temp->val = v[idx++];
+        //     temp = temp->next;
+        // }
+        // return head;
+
+
+        if(head == nullptr || head->next ==nullptr) return head;
+        ListNode* odd = head;
+        ListNode* even = head->next; 
+        ListNode* evenid = even;
+        while(even != nullptr && even->next != nullptr){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+
+            odd = odd->next;  even = even->next;
         }
-        if(temp != NULL) v.push_back(temp->val);
-        temp = head->next;
-        while(temp != NULL && temp->next != NULL){
-            v.push_back(temp->val);
-            temp = temp->next->next;
-        }
-        if(temp != NULL) v.push_back(temp->val);
-        temp = head; int idx =0;
-        while(temp != NULL){
-            temp->val = v[idx++];
-            temp = temp->next;
-        }
+        odd->next = evenid;
         return head;
     }
 };
