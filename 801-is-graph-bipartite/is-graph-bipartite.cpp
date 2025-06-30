b948bea05cf9e8e3264ab11794695f59ec732f7e
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-06-30 20:18:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (52.00%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -6,7 +6,6 @@ public:
         vis[node] = color;
         while(!q.empty()){
             int node = q.front();
-            // color = 1 - color;
             q.pop();
             for(auto it: adj[node]){
                 if(vis[it] == -1){
@@ -18,12 +17,22 @@ public:
         }
         return true;
     }
+    bool dfs(int node, int color, vector<int>& vis, vector<vector<int>>& adj){
+        vis[node] = color;
+        for(auto it: adj[node]){
+            if(vis[it] == -1){
+                if(dfs(it,!vis[node], vis, adj) == false) return false;
+            }
+            else if(vis[it] == vis[node]) return false;
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& adj) {
         int n = adj.size();
         vector<int> vis(n,-1);
         for(int i=0; i<n; i++){
             if(vis[i] == -1){
-               if(bfs(i,0,vis,adj) == false) return false;
+               if(dfs(i,0,vis,adj) == false) return false;
             }
         }
         return true;
