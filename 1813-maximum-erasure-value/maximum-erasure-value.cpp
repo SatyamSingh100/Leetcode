6f705a3a1a0c3f07f637cf3c5cb559f0702b4762
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-07-22 09:28:01 +0530

    Time: 165 ms (55.77%) | Memory: 131 MB (43.04%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& a) {
-        int n = a.size(),sum=0,ans=0,last=0;
-        unordered_map<int,int> m;
-        vector<int> pre(n+1,0);
-        pre[0] = 0;
+        int n = a.size(),sum=0,ans=0,st=0;
+        // unordered_map<int,int> m;
+        // vector<int> pre(n+1,0);
+        // pre[0] = 0;
+        // for(int i=0; i<n; i++){
+        //     pre[i+1] = pre[i] + a[i]; 
+        // }
+        // for(int i=0; i<n; i++){
+        //     if(m.find(a[i]) == m.end()){
+        //         sum += a[i]; m[a[i]] = i+1;
+        //     }
+        //     else{
+        //         if(st > m[a[i]]){
+        //             sum += a[i]; m[a[i]] = i+1;
+        //         }
+        //         else{
+        //             sum += a[i];
+        //             sum -= (pre[m[a[i]]] - pre[last]); 
+        //             st = m[a[i]];  m[a[i]] = i+1;
+        //         }
+        //     }
+        //     ans = max(ans,sum);
+        // }
+        // return ans;
+
+        unordered_set<int> s;
         for(int i=0; i<n; i++){
-            pre[i+1] = pre[i] + a[i]; 
-        }
-        for(int i=0; i<n; i++){
-            if(m.find(a[i]) == m.end()){
-                sum += a[i]; m[a[i]] = i+1;
+            if(s.find(a[i]) == s.end()){
+                sum += a[i]; s.insert(a[i]);
             }
             else{
-                if(last > m[a[i]]){
-                    sum += a[i]; m[a[i]] = i+1;
+                while(a[st] != a[i]){
+                    s.erase(a[st]); sum -= a[st];
+                    st++;
                 }
-                else{
-                    sum += a[i];
-                    sum -= (pre[m[a[i]]] - pre[last]); 
-                    last = m[a[i]];  m[a[i]] = i+1;
-                }
+                s.erase(a[st]); sum -= a[st]; st++;
+                sum += a[i]; s.insert(a[i]);
             }
             ans = max(ans,sum);
         }
