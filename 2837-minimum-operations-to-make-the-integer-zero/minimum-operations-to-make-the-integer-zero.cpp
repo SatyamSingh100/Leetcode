de5d29d2fd35cda10c009b382a2066626a31cf65
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-09-06 04:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (85.04%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int makeTheIntegerZero(int num1, int num2) {
         if(num2 > num1) return -1;
-        int t = 0;
+        int t = 1;
         while(true){
             long long val = (long long)num1 - (long long)t*num2;
             if(val < 0) return -1;
