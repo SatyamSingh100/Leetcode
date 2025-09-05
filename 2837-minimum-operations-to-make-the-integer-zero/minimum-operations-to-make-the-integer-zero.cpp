5361ef41f0a6f07c8dfd6b093f9ec14add59fc54
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-09-06 04:27:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (11.02%) - LeetSync

---
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+        if(num2 > num1) return -1;
+        int t = 0;
+        while(true){
+            long long val = (long long)num1 - (long long)t*num2;
+            if(val < 0) return -1;
+            int mini = __builtin_popcountll(val);
+            if(t >= mini && t<= val) return t;
+            t++;
+        }
+       return -1;
+    }
+};
