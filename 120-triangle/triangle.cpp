e7bf6a032b18f95d1674d6d159e0f70d45faf033
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-09-25 14:09:40 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (55.55%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,45 +1,16 @@
 class Solution {
 public:
-    int f(vector<vector<int>>& a, int i, int j){
-        int n = a.size();
-        if(i == n-1) return a[i][j];
-        
-        int down = a[i][j] + f(a,i+1,j);
-        int dright = a[i][j] + f(a,i+1,j+1);
-
-        return min(down,dright);
-    }
     int minimumTotal(vector<vector<int>>& a) {
-        int n=  a.size();
-        int m = a[n-1].size();
-
-        vector<vector<int>> dp(m,vector<int>(n,0));
-        for(int j=0; j<m; j++) dp[n-1][j] = a[n-1][j];
-
+        int n = a.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int j=0; j<n; j++) dp[n-1][j] = a[n-1][j];
         for(int i=n-2; i>=0; i--){
             for(int j=0; j<=i; j++){
-                int up = a[i][j] + dp[i+1][j];
-                int left = a[i][j] + dp[i+1][j+1];
-
-                dp[i][j] = min(up,left);
+                dp[i][j] = a[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+                // if(j > 0) dp[i][j] = min(a[i][j] + dp[i+1][j-1], dp[i][j]);
+                
             }
         }
         return dp[0][0];
-        // int ans = f(a,0,0);
-        // return ans;
-
-
-        // vector<vector<int>> dp(n,vector<int>(n,0));
-        // for(int i=0; i<m; i++){
-        //     dp[n-1][i] = a[n-1][i];
-        // }
-        // for(int i=n-2; i>=0; i--){
-        //     for(int j=0; j<=i; j++){
-        //         int ae = a[i][j] + dp[i+1][j];
-        //         int b= a[i][j] + dp[i+1][j+1];
-        //         dp[i][j] = min(ae,b);
-        //     }
-        // }
-        // return dp[0][0];
     }
 };
