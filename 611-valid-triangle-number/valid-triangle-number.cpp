1fc62e0771865674fef4746e2962c6c3fc57b542
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-09-26 21:35:02 +0530

    Time: 27 ms (89.51%) | Memory: 16.7 MB (42.88%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -4,16 +4,16 @@ public:
         int n=a.size(),i=0,cnt=0;
         sort(a.begin(),a.end());
         if(n <= 2) return 0;
-        while(i < n-2){
-        int j = i+1;
-        while(j<n-1){
-            int k = j+1;
-            while(k<n && a[k] < a[i] + a[j]){
-                k++; cnt++;
+        for(int i=n-1; i>=2; i--){
+            int l=0,r=i-1;
+            while(l<r){
+                int sum = a[l] + a[r];
+                if(sum > a[i]){
+                    cnt += (r-l);
+                    r--;
+                }
+                else l++;
             }
-            j++;
-        }
-        i++;
         }
         return cnt;
     }
