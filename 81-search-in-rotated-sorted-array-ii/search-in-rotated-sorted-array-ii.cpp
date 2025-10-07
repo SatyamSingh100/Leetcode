e2b23ce2368f95b55c4c234528072971ce81625a
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2025-10-07 19:50:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (55.32%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    bool search(vector<int>& a, int tar) {
-        int n = a.size(),i=0,j=n-1;
-        while(i <= j){
-            int mid = (j-i)/2 + i;
-            if(a[mid] == tar) return true;
-            else if(a[i] == a[mid] && a[j] == a[mid]){
-                i++; j--;
+    bool search(vector<int>& a, int k) {
+        int n = a.size();
+        if(n == 1) return a[0] == k;
+        int l = 0, r = n-1;
+        while(l <= r){
+            int mid =  l + (r-l)/2;
+            if(a[mid] == k) return true;
+            else if(a[mid] ==  a[l] && a[r] == a[mid]){
+                l++; r--;
             }
-            else if(a[i] <= a[mid]){
-                if(tar >= a[i] && tar <= a[mid]) j = mid - 1;
-                else i = mid + 1;
+            else if(a[mid] <= a[r]){
+                if(k <= a[r] && k > a[mid]) l = mid + 1;
+                else r = mid - 1;
             }
             else{
-                if(tar >= a[mid] && tar <= a[j]) i = mid + 1;
-                else j = mid -1;
+                if(k < a[mid] && k >= a[l]) r = mid - 1;
+                else l = mid + 1;
             }
         }
         return false;
