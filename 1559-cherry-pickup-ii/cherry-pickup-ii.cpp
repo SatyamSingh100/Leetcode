f412548aa62e5273d5be8e14b3aef225ba6d360c
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2026-02-01 09:33:22 +0530

    Time: 74 ms (19.73%) | Memory: 21 MB (54.01%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
-    int f(int i, int j1,int j2, vector<vector<int>>& a, vector<vector<vector<int>>>& dp){
-        int n = a.size(), m = a[0].size(),num=0;
-        if(j1 < 0 || j1 > m-1 || j2 < 0 || j2 > m-1) return 0;
-        if(i == n-1){
-            if(j1 == j2) return a[i][j1];
-            else return a[i][j1] + a[i][j2];
-        }
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
-
-        for(int dj1 = -1; dj1 <= 1; dj1++){
-            for(int dj2 = -1; dj2 <= 1; dj2++){
-                int ans = 0;
-                if(j1 == j2) ans = a[i][j1];
-                else ans = a[i][j1] + a[i][j2];
-                ans += f(i+1,j1 + dj1, j2 + dj2,a,dp);
-                num = max(num,ans);
+    int cherryPickup(vector<vector<int>>& a) {
+        int n = a.size(),m = a[0].size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m,vector<int>(m,0)));
+        for(int j1 = 0; j1 < m; j1++){
+            for(int j2 = 0; j2 < m; j2++){
+                if(j1 == j2) dp[n-1][j1][j2] = a[n-1][j1];
+                else dp[n-1][j1][j2] = a[n-1][j1] + a[n-1][j2];
             }
         }
-        dp[i][j1][j2] = num;
-        return num;
-    }
-    int cherryPickup(vector<vector<int>>& a) {
-        int n= a.size(), m = a[0].size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m, vector<int>(m,-1)));
-        int ans = f(0,0,m-1,a,dp);
-        return ans;
+
+        for(int i=n-2; i>=0; i--){
+            for(int j1=0; j1 <m; j1++){
+                for(int j2 = 0; j2<m; j2++){
+                    int val = -1e8;
+                    for(int dj1 = -1; dj1 <=1; dj1++){
+                        for(int dj2 = -1; dj2<=1; dj2++){
+                            int num =0;
+                            if(j1 == j2) num = a[i][j1];
+                            else num = a[i][j1] + a[i][j2];
+
+                            if(j1 + dj1 >= 0 && j1 + dj1 < m && j2 + dj2 >= 0 && j2 + dj2 < m){
+                                num += dp[i+1][j1+dj1][j2+dj2];
+                            }
+                            else num += -1e8;
+                            val = max(val,num);
+                        }
+                    }
+                    dp[i][j1][j2] = val;
+                }
+            }
+        }
+        return dp[0][0][m-1];
     }
 };
