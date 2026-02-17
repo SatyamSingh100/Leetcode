aeed84d3fb491f7be8046c22dc6e83a7dba9d3e2
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2026-02-17 15:59:39 +0530

    Time: 3 ms (9.95%) | Memory: 9.5 MB (85.28%) - LeetSync

---
+++ 401-binary-watch/binary-watch.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int num) {
+        int i=0;
+        vector<int> a = {0,1,2,3,4,5,6,7,8,9,10,11};
+        vector<int> b = {0};
+        for(int i=1; i<=59; i++) b.push_back(i);
+        int n1 = a.size(), n2 = b.size();
+        vector<string> ans;
+
+        while(i < n1){
+            int j =0; 
+            int num2 = num - __builtin_popcount(a[i]);
+            while(j < n2){
+                int val = __builtin_popcount(b[j]);
+                if(val == num2){
+                    string s ="";
+                    s += to_string(a[i]);
+                    s += ':';
+                    if(b[j] <= 9){
+                        s += '0'; s += b[j] + 48;
+                    }
+                    else{
+                        s += to_string(b[j]);
+                    }
+                    ans.push_back(s);
+                }
+                j++;
+            }
+            i++;
+        }
+        return ans;
+    }
+};
