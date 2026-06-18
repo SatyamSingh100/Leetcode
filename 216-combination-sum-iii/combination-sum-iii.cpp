8cb6a8d891841ff6a98bfade18c61d1901050469
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2026-06-18 08:32:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (60.64%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    void f1(vector<int>& a, int idx, int sum, vector<int>& b,vector<vector<int>>& c){
+    void f1(vector<int>& a, int idx, int sum, vector<int>& b,vector<vector<int>>& c,int k){
         if(sum == 0){
-            c.push_back(b); return;
+            if(b.size() == k) c.push_back(b);
+             return;
         }
         if(sum < 0 || idx == a.size()) return;
         
         b.push_back(a[idx]);
-        f1(a,idx+1,sum - a[idx], b,c);
+        f1(a,idx+1,sum - a[idx], b,c,k);
         b.pop_back();
-        f1(a,idx+1,sum,b,c);
+        f1(a,idx+1,sum,b,c,k);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> a ={1,2,3,4,5,6,7,8,9};
         vector<int> b;
         vector<vector<int>> c,ans;
-        f1(a,0,n,b,c);
+        f1(a,0,n,b,c,k);
         for(auto it: c){
             if(it.size() == k) ans.push_back(it);
         }
-        return ans;
+        return c;
         
     }
 };
