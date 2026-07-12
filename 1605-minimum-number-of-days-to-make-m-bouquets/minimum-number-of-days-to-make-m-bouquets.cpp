1e6a867c299b2f52ba234106d17a2f4fc5a6293d
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2026-07-13 04:41:12 +0530

    Time: 35 ms (9.12%) | Memory: 123.4 MB (5.32%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    int f1(vector<int>& a, int k, int d){
-        long long sum =0,cnt=0;
-        for(int i=0; i<a.size(); i++){
-            bool check = true;
-            if(a[i] <= d) cnt++;
-            else check = false;
-            if(cnt == k){
-                sum++; check = false;
+    bool check(vector<int> a, int mid,int k,int m){
+        int n = a.size(),cnt=0,bouq=0;
+        for(int i=0; i<n; i++){
+            if(bouq == k){
+                cnt++; bouq = 0;
             }
-            if(!check) cnt = 0;
+            if(a[i] <= mid) bouq++;
+            else bouq=0;
         }
-        return sum;
+        if(bouq == k) cnt++;
+        if(cnt >= m) return true;
+        return false; 
     }
     int minDays(vector<int>& a, int m, int k) {
-        int n = a.size();
-        long long  l = *min_element(a.begin(),a.end()), r = *max_element(a.begin(),a.end());
-        long long ans = 1e9 + 11;
-        while(l <=  r){
-            long long mid = (l+r)/2;
-            long long num = f1(a,k,mid);
-            if(num >= m){
-                ans = min(ans,mid); r = mid -1;
+        int l =1e9, r=0, n = a.size(),ans=-1;
+        long long num = m;
+        num = num * k;
+        if(num > n) return ans;
+        for(int i=0; i<n; i++){
+            l = min(l,a[i]);
+            r = max(r,a[i]);
+        }
+        while(l <= r){
+            int mid = (l + r)/2;
+            if(check(a,mid,k,m)){
+                ans = mid;
+                r = mid - 1;
             }
             else l = mid + 1;
         }
-        if(ans == 1e9 + 11) ans = -1;
         return ans;
     }
 };
