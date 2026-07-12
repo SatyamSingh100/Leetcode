d0f6ad903e0a725d9582c3ef97a4a27e388e89e0
SatyamSingh100 <153487356+SatyamSingh100@users.noreply.github.com>
2026-07-12 06:07:43 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (34.82%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int search(vector<int>& a, int tar) {
-        int n = a.size(),i=0,j=n-1,ans=-1;
+    int search(vector<int>& a, int target) {
+        int n = a.size(),idx =-1;
+        int i= 0, j= n-1;
         while(i <= j){
-            int mid = (j-i)/2 + i;
-            if(a[mid] == tar){
-                ans = mid; break;
-            }
-            else if(a[i] <= a[mid]){
-                if(tar >= a[i] && tar <= a[mid]) j = mid -1;
+            int mid = (i + j)/2;
+            if(a[mid] == target) return mid;
+            else if(a[i] <= a[mid]){                      // Left Half sorted
+                if(target >= a[i] && target < a[mid]) j = mid - 1;
                 else i = mid + 1;
             }
             else{
-                if(tar >=  a[mid] && tar <= a[j]) i = mid + 1;
+                if(target > a[mid] && target <= a[j]) i = mid + 1;
                 else j = mid - 1;
             }
         }
-        return ans;
+        return idx;
     }
 };
